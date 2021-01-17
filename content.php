57383da7fc093aa8f9a265c8d5067c9d643ac4e2
kevinklaam <46432275+kevinklaam@users.noreply.github.com>
2021-01-17 01:54:29 -0500

    Add files via upload

--- content.php
+++ content.php
@@ -1,15 +1,35 @@
 <?php
 header('Content-Type: text/javascript; charset=UTF-8');
 
-$imageFolder = 'https://kevinklaam.github.io/img/';
+$imageFolder = 'img/';
 $imageTypes = '{*.jpg,*.JPG,*.jpeg,*.JPEG,*.png,*.PNG}';
 
+$url='https://api.github.com/repos/kevinklaam/imagegallery/git/trees/main?format=json';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_USERAGENT,'kevinklaam');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
+$result=curl_exec($ch);
+curl_close($ch);
+
+$json = json_decode($result, true);
+
+$result2 = [];
+array_walk_recursive($json, function($value, $key) use(&$result2) {
+    if ($key === 'path') {
+        $result2[] = 'https://raw.githubusercontent.com/kevinklaam/imagegallery/main/' . $value;
+    }
+});
+
+//var_dump($result2);
+
 # Create images array
-$images = glob($imageFolder . $imageTypes, GLOB_BRACE);
+//$images = glob($imageFolder . $imageTypes, GLOB_BRACE);
+//var_dump($images);
 
 # Generate the HTML output
 writehtml('<ul class="content">');
-foreach ($images as $image) {
+foreach ($result2 as $image) {
 
     # Get the name of the image, stripped from image folder path and file type extension
     $name = 'Image name: ' . substr($image, strlen($imageFolder), strpos($image, '.') - strlen($imageFolder));
